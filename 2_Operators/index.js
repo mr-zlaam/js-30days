93413e05b4ef77fac84e32eecb12e9c055c71749
Zlaam <mrzalaam@gmail.com>
2024-07-14 12:32:01 +0500

    feat: create 2_Operators/index.js

---
+++ 2_Operators/index.js
@@ -0,0 +1,99 @@
+/*  OPERATORS */
+
+/* Activity 1: Arithmetic Operations */
+
+// * Q1= Write a program  to add two numbers and log the result to the console
+
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(sum);
+
+// * Q2= Write a program to subtract two numbers and log the result to the console
+
+let num3 = 5;
+let num4 = 10;
+let difference = num3 - num4;
+console.log(difference);
+
+// * Q3= Write a program to multiply two numbers and log the result to the console
+
+let num5 = 5;
+let num6 = 10;
+let product = num5 * num6;
+console.log(product);
+
+// * Q4= Write a program to divide two numbers and log the result to the console
+
+let num7 = 5;
+let num8 = 10;
+let quotient = num7 / num8;
+console.log(quotient);
+
+// * Q5= Write a program to calculate the remainder of two numbers and log the result to the console
+
+let num9 = 5;
+let num10 = 10;
+let remainder = num9 % num10;
+console.log(remainder);
+
+/* Activity 2: Assignment Operators */
+
+// * Q1= Use the += operator to add the number  to variable and log the result to the console
+
+let num11 = 5;
+num11 += 10;
+console.log(num11); // 15
+
+// * Q2= Use the -= operator to subtract the number  from variable and log the result to the console
+
+let num12 = 5;
+num12 -= 10;
+console.log(num12); // -5
+
+/* Activity 3: Comparison Operators */
+
+// * Q1= Write a program to compare  two numbers using > and < log the result to console
+
+let num13 = 5;
+let num14 = 10;
+let comparison = num13 > num14;
+console.log(comparison);
+
+// * Q2= Write a program to compare  two numbers using >= and <= log the result to console
+
+let num15 = 5;
+let num16 = 10;
+let comparison2 = num15 >= num16;
+console.log(comparison2);
+
+/* Activity 4: Logical Operators */
+
+// * Q1= Write a program to compare  two numbers using && and || log the result to console
+
+let num17 = 5;
+let num18 = 10;
+let comparison3 = num17 < num18;
+console.log(comparison3);
+
+// * Q2= Write a program to compare  two numbers using == and === log the result to console
+
+let num19 = 5;
+let num20 = 10;
+let comparison4 = num19 == num20;
+console.log(comparison4);
+
+// * Q3= Write a program to compare  two numbers using != and !== log the result to console
+
+let num21 = 5;
+let num22 = 10;
+let comparison5 = num21 != num22;
+console.log(comparison5);
+
+/* Activity 5: Ternary Operator */
+
+// * Q1= Write a program to that uses ternary operators to check  if a number is positive or negative and log the result to console
+
+let num23 = 5;
+let result = num23 > 0 ? "positive" : "negative";
+console.log(result);
